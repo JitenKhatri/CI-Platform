83db5d325f0be980007f6fbc087ef9c473ccd874
Jiten Khatri <jitenkhatri81@gmail.com>
2023-02-10 17:05:22 +0530

    Created Login page in MVC UI

---
+++ CI_Platform_MVC/CI_Platform/Controllers/UserAuthenticationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_Platform.Controllers
+{
+    public class UserAuthenticationController : Controller
+    {
+        public IActionResult login()
+        {
+            return View();
+        }
+        //public IActionResult registration()
+        //{
+        //    return View();
+        //}
+        //public IActionResult lostPassword()
+        //{
+        //    return View();
+        //}
+        //public IActionResult resetPassword()
+        //{
+        //    return View();
+        //}
+    }
+}
