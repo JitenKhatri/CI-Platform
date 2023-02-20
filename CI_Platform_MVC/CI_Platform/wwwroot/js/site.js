98831a62bb076e334bb6cc16a2335ad4f679bf03
Jiten Khatri <jitenkhatri81@gmail.com>
2023-02-20 18:17:48 +0530

    Minor changes and mission-not-found page added

--- CI_Platform_MVC/CI_Platform/wwwroot/js/site.js
+++ CI_Platform_MVC/CI_Platform/wwwroot/js/site.js
@@ -1,56 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-// // Listen for the click event on the dropdown items
-// $(".dropdown-menu a").click(function() {
-//     // Get the text of the selected item
-//     var selectedItem = $(this).text();
-
-//     // Add the selected item to the selected filters list
-//     var selectedFilters = $("#selectedFilters").html();
-//     $("#selectedFilters").html(selectedFilters + "<span class='badge badge-secondary ml-1'>" + selectedItem + "<button type='button' class='close ml-1' aria-label='Close'><span aria-hidden='true'>&times;</span></button></span>");
-//   });
-
-//   // Listen for the click event on the close buttons within the selected filters
-//   $("#selectedFilters").on("click", "button.close", function() {
-//     // Remove the selected filter from the selected filters list
-//     $(this).closest("span").remove();
-//   });
-
-//   // Listen for the click event on the clear filters button
-//   $("#clearFiltersButton").click(function() {
-//     // Remove all the selected filters from the selected filters list
-//     $("#selectedFilters").empty();
-//   });
-// const toggleViewButton = document.querySelector('.toggle-view-btn');
-// const productItems = document.querySelectorAll('.product-item');
-
-// toggleViewButton.addEventListener('click', () => {
-//   if (toggleViewButton.dataset.view === 'grid') {
-//     toggleViewButton.innerHTML = '<i class="bi bi-list"></i> List View';
-//     toggleViewButton.dataset.view = 'list';
-//     productItems.forEach(item => {
-//       item.classList.remove('col-md-4');
-//       item.classList.remove('col-sm-6');
-//       item.classList.add('col-12');
-//     });
-//   } else {
-//     toggleViewButton.innerHTML = '<i class="bi bi-grid-3x3"></i> Grid View';
-//     toggleViewButton.dataset.view = 'grid';
-//     productItems.forEach(item => {
-//       item.classList.add('col-md-4');
-//       item.classList.add('col-sm-6');
-//       item.classList.remove('col-12');
-//     });
-//   }
-// });
-// const addFavoriteButtons = document.querySelectorAll('.add-favorite');
-// addFavoriteButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     button.classList.toggle('active');
-//   });
-// });
+﻿
 
 
 function showList(e) {
@@ -67,47 +15,29 @@ function gridList(e) {
 $(document).on('click', '.btn-grid', gridList);
 $(document).on('click', '.btn-list', showList);
 
-// filters js 
-$(document).ready(function () {
-    // handle country dropdown
-    $('#country').change(function () {
-        var value = $(this).val();
-        if (value != '') {
-            $('#selected-filters').append('<div class="col-md-2 filter-item">' + value + '<button class="btn btn-sm btn-danger remove-filter" data-filter="country">&times;</button></div>');
-        }
+
+
+// Dropdown item click event listener
+$('.dropdown-item').on('click', function () {
+    const dropdown = $(this).closest('.dropdown');
+    const dropdownTitle = dropdown.find('.dropdown-toggle').text();
+    const value = $(this).data('value');
+    const selectedItemsRow = $('#selected-items-row');
+
+    // Create a badge element to display the selected item
+    const badge = $('<span class="badge badge-pill filter-box ml-2"></span>').text(value);
+    const cross = $('<span class="badge badge-pill badge-secondary ml-2" id="cross-btn"></span>').html('&times;');
+    badge.append(cross);
+    selectedItemsRow.append(badge);
+
+    // Cross button click event listener
+    cross.on('click', function () {
+        //    $(this).closest('.badge').remove();
+        badge.remove();
     });
 
-    // handle city dropdown
-    $('#city').change(function () {
-        var value = $(this).val();
-        if (value != '') {
-            $('#selected-filters').append('<div class="col-md-2 filter-item">' + value + '<button class="btn btn-sm btn-danger remove-filter" data-filter="city">&times;</button></div>');
-        }
-    });
-    // handle theme dropdown
-    $('#theme').change(function () {
-        var value = $(this).val();
-        if (value != '') {
-            $('#selected-filters').append('<div class="col-md-2 filter-item">' + value + '<button class="btn btn-sm btn-danger remove-filter" data-filter="theme">×</button></div>');
-        }
-    });
-    // handle skill dropdown
-    $('#skill').change(function () {
-        var value = $(this).val();
-        if (value != '') {
-            $('#selected-filters').append('<div class="col-md-2 filter-item">' + value + '<button class="btn btn-sm btn-danger remove-filter" data-filter="skill">×</button></div>');
-        }
-    });
-    // handle remove filter button click
-    $(document).on('click', '.remove-filter', function () {
-        var filter = $(this).data('filter');
-        $('#' + filter).val('');
-        $(this).parent().remove();
-    });
-
-    // handle clear filters button click
-    $('#clear-filters').click(function () {
-        $('#country, #city, #theme, #skill').val('');
-        $('#selected-filters').empty();
+    // Clear all button click event listener
+    $('#clear-all-btn').on('click', function () {
+        selectedItemsRow.empty();
     });
 });
